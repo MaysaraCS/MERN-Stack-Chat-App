b0a480f271928f1756e3183728c8787585d509e5
Maysara <maysaracs1001@gmail.com>
2025-10-13 16:03:50 +0300

    project structure vite

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePages from './pages/HomePages.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePages />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/HomePages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePages = () => {
+  return (
+    <div><h1>Home Page</h1></div>
+  )
+}
+
+export default HomePages
