a3cf74613c35ca815f7bc80cab8835fbf6599a67
Maysara <maysaracs1001@gmail.com>
2025-10-13 17:46:18 +0300

    side bar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] 
+    bg-contain"> 
       <Routes>
         <Route path='/' element={<HomePages />} />
         <Route path='/login' element={<LoginPage />} />
---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSideBar = () => {
+  return (
+    <div><h1>RightSideBar</h1></div>
+  )
+}
+
+export default RightSideBar
