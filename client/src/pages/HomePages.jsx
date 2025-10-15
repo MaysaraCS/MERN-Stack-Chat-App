1cd111768c54c4df481576089151fa73501068f9
Maysara <maysaracs1001@gmail.com>
2025-10-15 17:06:23 +0300

    final test before deployment

--- client/src/pages/HomePages.jsx
+++ client/src/pages/HomePages.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSideBar from '../components/RightSideBar'
+import { ChatContext } from '../context/ChatContext'
 const HomePages = () => {
 
-    const [selectedUser, setSelectedUser] = useState(null)
+    const { selectedUser } = useContext(ChatContext)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
         overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}  `}>
-            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-            {selectedUser && <RightSideBar selectedUser={selectedUser} />}
+            <Sidebar />
+            <ChatContainer/>
+            <RightSideBar />
         </div>
 
     </div>
